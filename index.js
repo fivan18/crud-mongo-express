db60d00667b21d66316ff2ea65e836afd91b5621
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-06-29 15:00:55 -0500

    add router

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-//index.js
 //Import Express
 let express = require('express')
+//Import routes
+let apiRoutes = require("./routes")
+
 //Start App
 let app = express();
 //Assign port
 var port = process.env.PORT || 8080;
+//Use API routes in the App
+app.use('/api', apiRoutes)
 // Welcome message
 app.get('/', (req, res) => res.send('Welcome to Express'));
 // Launch app to the specified port
 app.listen(port, function() {
     console.log("Running CRUD express-mongo on Port "+ port);
 })
+
+
---
+++ routes.js
@@ -0,0 +1,11 @@
+//initialize express router
+let router = require('express').Router();
+//set default API response
+router.get('/', function(req, res) {
+    res.json({
+        status: 'API Works',
+        message: 'Welcome to express-mongo API'
+    });
+});
+//Export API routes
+module.exports = router;
