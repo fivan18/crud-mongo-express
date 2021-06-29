1196b9f09f213a7d03a4eb0bbe878ca02bafdf0d
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-06-29 17:12:01 -0500

    refactor index.js config, body parser didn't work

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let express = require('express')
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let apiRoutes = require("./routes");
+
+
 let app = express();
 var port = process.env.PORT || 8080;
 
-// Welcome message
+// Welcome message in the root path
 app.get('/', (req, res) => res.send('Welcome to Express'));
+
 // Launch app to the specified port
 app.listen(port, function() {
     console.log("Running mongo-express on Port "+ port);
-})
-//Import routes
-let apiRoutes = require("./routes")
-//Use API routes in the App
-app.use('/api', apiRoutes)
-//import body parser
-let bodyParser = require('body-parser');
-//import mongoose
-let mongoose = require('mongoose');
-//configure bodyparser to hande the post requests
+});
+
+//configure bodyparser to handle the post requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
 //connect to mongoose
 const dbPath = 'mongodb://localhost/firstrest';
 const options = {useNewUrlParser: true, useUnifiedTopology: true}
@@ -29,4 +29,7 @@ mongo.then(() => {
     console.log('connected');
 }, error => {
     console.log(error, 'error');
-})
+});
+
+//Use API routes in the App
+app.use('/api', apiRoutes);
