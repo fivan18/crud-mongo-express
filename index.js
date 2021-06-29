1a5e6a907f3c9870a51df8165d176d44f7dec119
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-06-29 15:20:46 -0500

    configure mongoose and body parser

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-//Import Express
 let express = require('express')
-//Import routes
-let apiRoutes = require("./routes")
-
-//Start App
 let app = express();
-//Assign port
 var port = process.env.PORT || 8080;
-//Use API routes in the App
-app.use('/api', apiRoutes)
+
 // Welcome message
 app.get('/', (req, res) => res.send('Welcome to Express'));
 // Launch app to the specified port
 app.listen(port, function() {
-    console.log("Running CRUD express-mongo on Port "+ port);
+    console.log("Running mongo-express on Port "+ port);
 })
-
-
+//Import routes
+let apiRoutes = require("./routes")
+//Use API routes in the App
+app.use('/api', apiRoutes)
+//import body parser
+let bodyParser = require('body-parser');
+//import mongoose
+let mongoose = require('mongoose');
+//configure bodyparser to hande the post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+//connect to mongoose
+const dbPath = 'mongodb://localhost/firstrest';
+const options = {useNewUrlParser: true, useUnifiedTopology: true}
+const mongo = mongoose.connect(dbPath, options);
+mongo.then(() => {
+    console.log('connected');
+}, error => {
+    console.log(error, 'error');
+})
